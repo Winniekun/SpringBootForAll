1ebd9ece9c8f63bbb91cd0e40fec8a49d1e8649e
weikunkun <kongwiki@163.com>
2020-10-28 23:00:58 +0800

    :sparkles: add es with springboot

---
+++ 07-es/src/main/java/com/wkk/es/config/EsConfig.java
@@ -0,0 +1,22 @@
+package com.wkk.es.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kongwiki@163.com
+ * @since 2020/10/28
+ */
+@Configuration
+public class EsConfig {
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")));
+        return client;
+    }
+}
---
+++ 07-es/src/test/java/com/wkk/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkk.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
